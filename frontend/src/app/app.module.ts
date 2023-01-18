10b0483deca51ec10e94d7908c91fa272d7d1e6a
kristine Hoyer <kristine.jincharadze@yahoo.com>
2023-01-18 10:33:18 +0100

    no changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WarenkorbComponent } from './warenkorb/warenkorb.component';
@@ -11,8 +9,11 @@ import { VersandComponent } from './versand/versand.component';
 import { KopfzeileComponent } from './kopfzeile/kopfzeile.component';
 import {RouterModule} from "@angular/router";
 import { ProduktAlertsComponent } from './produkt-alerts/produkt-alerts.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { LogInComponent } from './log-in/log-in.component';
+import { RegistrierenComponent } from './registrieren/registrieren.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
@@ -22,20 +23,25 @@ import {HttpClientModule} from "@angular/common/http";
     ProduktListeComponent,
     VersandComponent,
     KopfzeileComponent,
-    ProduktAlertsComponent
+    ProduktAlertsComponent,
+    LogInComponent,
+    RegistrierenComponent,
+    ServicesComponent
   ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    RouterModule.forRoot([
-      { path: '', component: ProduktListeComponent },
-      {path: 'produkts/:produktId', component: ProduktBeschreibungComponent},
-      {path: 'warenkorb', component: WarenkorbComponent},
-      {path: 'versand', component: VersandComponent },
-    ])
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        RouterModule.forRoot([
+            {path: '', component: ProduktListeComponent},
+            {path: 'produkts/:produktId', component: ProduktBeschreibungComponent},
+            {path: 'warenkorb', component: WarenkorbComponent},
+            {path: 'versand', component: VersandComponent},
+            {path: 'services', component: ServicesComponent},
+        ]),
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/versand/versand.component.ts
+++ frontend/src/app/versand/versand.component.ts
@@ -9,7 +9,9 @@ import {Observable} from "rxjs";
   styleUrls: ['./versand.component.css']
 })
 export class VersandComponent  implements OnInit{
-  constructor(private warenkorbService: WarenkorbService) {}
+
+  constructor(private warenkorbService: WarenkorbService)
+  {}
   versandKosten!: Observable<{type: string, price: number}[]>;
   addToWarenkorb1(produkt: any) {
     this.warenkorbService.addToWarenkorb(produkt);
