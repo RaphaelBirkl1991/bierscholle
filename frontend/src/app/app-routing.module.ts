c11ccac1122f074219110d9715dc5b4d7876501d
RaphaelBirkl1991 <raphael.birkl@gmail.com>
2023-01-19 21:04:13 +0100

    We are back :)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProduktListeComponent} from "./produkt-liste/produkt-liste.component";
+import {KopfzeileComponent} from "./kopfzeile/kopfzeile.component";
+import {WarenkorbComponent} from "./warenkorb/warenkorb.component";
+import {KundenListeComponent} from "./kunden-liste/kunden-liste.component";
+import {HauptseiteComponent} from "./hauptseite/hauptseite.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'produkt_liste', component: ProduktListeComponent},
+  {path: 'kopfzeile', component: KopfzeileComponent},
+  {path: 'warenkorb', component: WarenkorbComponent},
+  {path: 'kunden_liste', component: KundenListeComponent},
+  {path: '', component: HauptseiteComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
