402a4d2d543bb1804fd857dd24d250e9247c118c
RaphaelBirkl1991 <raphael.birkl@gmail.com>
2023-01-20 12:49:44 +0100

    During Lunchtime Commit. registrieren works!

--- frontend/src/app/registrieren/registrieren.component.ts
+++ frontend/src/app/registrieren/registrieren.component.ts
@@ -1,46 +1,59 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/services.component';
+import {Kunde} from "../kunde";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-registrieren',
   templateUrl: './registrieren.component.html',
   styleUrls: ['./registrieren.component.css']
 })
+
+
 export class RegistrierenComponent {
 
 
-  vorname = '';
-  nachname = '';
-  strasse = '';
-  hausnummer = 0;
-  plz = 0;
-  ort = '';
-  email = '';
-  passwort = '';
-  passwortBest = '';
+  kunden: Kunde[] = [];
+  neuerKunde: Kunde = {vorname: '', nachname: '', strasse: '', hausnummer: 0, plz: 0, ort: '', email: '', passwort:''};
 
 
-  constructor(private authService: AuthService, private router: Router) {}
+  //constructor(private authService: AuthService, private router: Router) {}
 
-  onSubmit(){
-
+  constructor(private http: HttpClient) {
   }
 
-  register() {
-    if (this.passwort !== this.passwortBest) {
-      alert('Passwords do not match');
-      return;
-    }
-
-    this.authService.register(this.email, this.passwort).subscribe(response => {
-      if (response.status === 201) {
-        this.router.navigate(['/login']);
-      } else {
-        alert('An error occurred');
-      }
-    });
+  ngOnInit(): void {
+   // this.http.get<Entry[]>('/api/guestbook').subscribe(e => this.entries = e);
   }
+
+
+ save() {
+    this.http.post<Kunde[]>('api/registrieren', this.neuerKunde).subscribe(k => this.kunden = k);
+ }
+
+
+
+
+
+
+
+
+  // Register Methode mit Passwort Vergleich
+
+  // register() {
+  //   if (this.passwort !== this.passwortBest) {
+  //     alert('Passwords do not match');
+  //     return;
+  //   }
+  //   this.authService.register(this.email, this.passwort).subscribe(response => {
+  //     if (response.status === 201) {
+  //       this.router.navigate(['/login']);
+  //     } else {
+  //       alert('An error occurred');
+  //     }
+  //   });
+  // }
 
 }
 
