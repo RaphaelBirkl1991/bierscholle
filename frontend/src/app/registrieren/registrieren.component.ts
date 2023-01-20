e3b68c7bc5bb7491134165b6189ce64796a5ec14
RaphaelBirkl1991 <raphael.birkl@gmail.com>
2023-01-20 09:59:09 +0100

    Formular registrieren ohne Logik erstellt.

--- frontend/src/app/registrieren/registrieren.component.ts
+++ frontend/src/app/registrieren/registrieren.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/services.component';
 
 @Component({
   selector: 'app-registrieren',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class RegistrierenComponent {
 
+
+  vorname = '';
+  nachname = '';
+  strasse = '';
+  hausnummer = 0;
+  plz = 0;
+  ort = '';
+  email = '';
+  passwort = '';
+  passwortBest = '';
+
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(){
+
+  }
+
+  register() {
+    if (this.passwort !== this.passwortBest) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    this.authService.register(this.email, this.passwort).subscribe(response => {
+      if (response.status === 201) {
+        this.router.navigate(['/login']);
+      } else {
+        alert('An error occurred');
+      }
+    });
+  }
+
 }
+
